9b9abcc1c29a6bf990e061fdc68f16b92acbaf04
Vinicios Miguel <vinicios.miguel@hiper.com.br>
2024-11-09 19:30:03 -0300

    alteração de tamanho do campo titulo do anúncio

---
+++ src/DoaFacil.Backend.Infra.Database/Migrations/20241109200453_AlteracaoTamanhoColunaTituloParaOitentaCaracteresNaTabelaAnuncio.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoaFacil.Backend.Infra.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteracaoTamanhoColunaTituloParaOitentaCaracteresNaTabelaAnuncio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "anuncio",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(40)",
+                oldMaxLength: 40)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "anuncio",
+                type: "varchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(80)",
+                oldMaxLength: 80)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
